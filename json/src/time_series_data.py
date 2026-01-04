7bbe91ed881f523dece25c1ed088598d0c997e26
mathew <matteogales@gmail.com>
2026-01-04 14:56:27 +0000

    Updating path used to retrieve time series data using the JSON API. Updating ape-python-client dependency.

--- json/src/time_series_data.py
+++ json/src/time_series_data.py
@@ -40,7 +40,7 @@ request = {
 
 # Send the request
 
-request_url = data_service + '/v1/time-series/data/get'
+request_url = data_service + '/v1/entity/time-series/get'
 response = requests.post(request_url, json=request)
 
 if response.status_code == 200:
