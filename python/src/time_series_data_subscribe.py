1de54402dd2824a54e5327ec31d60643c35765e6
mathew <matteogales@gmail.com>
2026-01-31 14:37:36 +0000

    Updating ape-python-client dependency to 0.0.116. Adding examples to illustrate how to subscribe to entity time series data. Renaming scripts giving examples of how to subscribe to device data.

---
+++ python/src/time_series_data_subscribe.py
@@ -0,0 +1,65 @@
+"""
+Subscribe all the streaming time series data for an entity that a user has access to.
+"""
+from ape_client import Client
+
+from datetime import datetime
+from typing import Dict, Any
+
+
+def data_received(message: Dict[str, Any]):
+    """
+    Called when data has been received.
+
+    :param message: Message received.
+    :type message: Dict[str, Any]
+    """
+    # Obtain the message header and body
+
+    header = message["header"]
+    body = message["body"]
+
+    # Obtain relevant metadata from the header
+
+    entity_code = header["entityCode"]
+
+    # Parse timestamp at a second accuracy
+
+    timestamp = body["timestamp"].split(".")[0]
+    timestamp = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S")
+
+    # Parse the field data
+
+    to_print = f"Time series data for the entity '{entity_code}' was published at '{timestamp.strftime("%H:%M:%S %d-%m-%Y")}': "
+    fields = body["fields"]
+    first = True
+
+    for field_data in fields:
+        # Obtain field name and value
+
+        field_name = field_data["name"]
+        field_value = field_data["value"]
+
+        # Append data to the text to print
+
+        if first:
+            first = False
+        else:
+            to_print = to_print + ", "
+
+        to_print = to_print + f"{field_name}={field_value}"
+
+    # Print out message
+
+    print(to_print)
+
+
+# Login to the platform
+
+client = Client()
+client.login()
+
+# Subscribe to streaming data
+
+client.subscribe(data_type="entity-time-series-data",
+                 data_received=data_received)
